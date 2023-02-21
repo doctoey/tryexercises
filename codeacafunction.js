4f9d741e53cf09bfada1cbe6f8b1a5ef8d32e6c9
doctoey <123890144+doctoey@users.noreply.github.com>
2023-02-22 03:23:09 +0700

    add Default Parameters

--- codeacafunction.js
+++ codeacafunction.js
@@ -3,3 +3,12 @@ function sayThanks(name) {
 }
 
 sayThanks('Cole');
+
+
+//Default Parameters
+function makeShoppingList(item1='milk', item2='bread', item3='eggs'){
+  console.log(`Remember to buy ${item1}`);
+  console.log(`Remember to buy ${item2}`);
+  console.log(`Remember to buy ${item3}`);
+}
+makeShoppingList();
