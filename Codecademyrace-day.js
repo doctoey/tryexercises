47a0211110c49e340da80aeedaa1423021f65ad7
doctoey <123890144+doctoey@users.noreply.github.com>
2023-02-22 03:01:35 +0700

    add see the registration desk

--- Codecademyrace-day.js
+++ Codecademyrace-day.js
@@ -2,7 +2,7 @@ let raceNumber = Math.floor(Math.random() * 1000);
 console.log(raceNumber)
 
 const registeredEarly = false;
-const runnerAge = 19;
+const runnerAge = 18;
 
 if (runnerAge>18 && registeredEarly){
   raceNumber += 1000;
@@ -15,4 +15,6 @@ if (runnerAge>18 && registeredEarly) {
   console.log(`Runner:${raceNumber}: Your will race at 11:00`);
 } else if (runnerAge < 18) {
   console.log(`Runner:${raceNumber}: Your will race at 12:30`)
+} else {
+ console.log("see the registration desk")
 }
