b9680827fbc4438b74bcce10370577ca17664c58
doctoey <123890144+doctoey@users.noreply.github.com>
2023-02-22 03:46:14 +0700

    add Helper Functions

--- codeacafunction.js
+++ codeacafunction.js
@@ -14,7 +14,6 @@ function makeShoppingList(item1='milk', item2='bread', item3='eggs'){
 makeShoppingList();
 
 
-
 //Return
  function monitorCount(rows, columns){
    let cal = rows * columns;
@@ -22,3 +21,27 @@ makeShoppingList();
  };
  const numOfMonitors = monitorCount(5, 4)
  console.log(numOfMonitors)
+
+
+//Helper Functions
+function multiplyByNineFifths(number) {
+  return number * (9/5);
+};
+ 
+function getFahrenheit(celsius) {
+  return multiplyByNineFifths(celsius) + 32;
+};
+ 
+getFahrenheit(15); // Returns 59
+
+//----------------------------------
+function monitorCounts(rows, columns) {
+  return rows * columns;
+};
+
+function costOfMonitors(rows, columns) {
+  return monitorCounts(rows, columns) * 200 ; 
+};
+
+const totalCost = costOfMonitors(5,4)
+console.log(totalCost)     //return 4000  5*4*200 ด้านบน
