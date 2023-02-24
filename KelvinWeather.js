b05bb546f262dabcf1773955557ec99922f34218
doctoey <123890144+doctoey@users.noreply.github.com>
2023-02-24 22:53:29 +0700

    Create KelvinWeather.js

---
+++ KelvinWeather.js
@@ -0,0 +1,10 @@
+const kelvin = 293; 
+//kelvin temp
+const celsius = kelvin - 273;
+console.log(`temp celsius is ${celsius}`)
+
+//cel = k-273
+Fahrenheit = (celsius * 1.8 + 32);
+//far
+Fahrenheit = Math.floor(Fahrenheit);
+console.log(`The temperature is ${Fahrenheit} degrees Fahrenheit.`)
