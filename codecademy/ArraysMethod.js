ceb80b068153b3f36c72a96c0c2c2240012d7637
doctoey <123890144+doctoey@users.noreply.github.com>
2023-02-25 00:54:57 +0700

    Create ArraysMethod.js

---
+++ codecademy/ArraysMethod.js
@@ -0,0 +1,39 @@
+//.push()  เพิ่มสมาชิกไปต่อท้าย
+const chores = ['wash dishes', 'do laundry', 'take out trash'];
+
+chores.push('itemadd1','itemadd2')
+console.log(chores)
+
+//.pop() เอาสมาชิกสุดท้ายออก
+const chores = ['wash dishes', 'do laundry', 'take out trash', 'cook dinner', 'mop floor'];
+
+chores.pop();
+console.log(chores)
+
+
+//.shift() เอาสมาชิกด้านหน้าออก
+const groceryList = ['orange juice', 'bananas', 'coffee beans', 'brown rice', 'pasta', 'coconut oil', 'plantains'];
+
+groceryList.shift()    //เอาสมาชิกด้านหน้าออก ในที่นี้ orange juice
+console.log(groceryList) //add สมาชิกเข้าไปด้านหน้า
+
+//.unshift() เพิ่มสมาชิกไปด้านหน้า
+groceryList.unshift('popcorn')
+console.log(groceryList) // add popcorn ไปข้างหน้า
+
+//slice ex1   
+const sldata = [100,200,300,400,500]  //ต้องการ 200 ,300
+sldata.slice(1,3) // จะได้ ชิ้นที่ 1 คือ 200 กับชิ้นที่ 2 (ทำการ+1) 300
+console.log(sldata);  // จะยังเป็นอันเดิมอยู่ว ไม่แก้ array 
+// ต้องหาตัวแปรมารับตัวที่จะเอาออกมา
+let lastdata = sldata.slice(1,3)
+console.log(lastdata);  // จะได้ 200,300 ละ
+
+//slice ex2 ของ codecademy
+console.log(groceryList.slice(1, 4)); // เอาตัวที่จะเอาตัวแรก , ตัวที่จะเอาตัวสุดท้าย +1 (first,end+1)
+
+console.log(groceryList); //[ 'bananas', 'coffee beans', 'brown rice' ]
+
+const pastaIndex = groceryList.indexOf('pasta');
+
+console.log(pastaIndex);
