3204654b911ca62b9fdc67f00c9adaf258fc9d6d
Cheewathun Lerttanapit <123890144+doctoey@users.noreply.github.com>
2023-03-04 22:43:59 +0700

    Create The-While-Loop.js

---
+++ codecademy/0study/The-While-Loop.js
@@ -0,0 +1,23 @@
+const cards = ['diamond', 'spade', 'heart', 'club'];
+
+// Write your code below
+let currentCard;
+
+while ( currentCard != 'spade') {
+  currentCard = cards[Math.floor(Math.random() * 4)];    //สุ่มเลขไปเรื่อยๆ 0 1 2 3 ถ้าเจอ 1 คือ spade ก็จะหยุด
+	console.log(currentCard);
+}
+
+
+
+// A for loop that prints 1, 2, and 3
+for (let counterOne = 1; counterOne < 4; counterOne++){
+  console.log(counterOne);
+}
+ 
+// A while loop that prints 1, 2, and 3
+let counterTwo = 1;
+while (counterTwo < 4) {
+  console.log(counterTwo);
+  counterTwo++;
+}
