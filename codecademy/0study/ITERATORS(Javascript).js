b19792777bb23e75cca3fe74d6a8ffc1aabcf5e5
Cheewathun Lerttanapit <123890144+doctoey@users.noreply.github.com>
2023-03-05 02:23:10 +0700

    Create ITERATORS(Javascript).js
    .forEach()
    .map()
    .filter()
    .findIndex()
    .reduce()

---
+++ codecademy/0study/ITERATORS(Javascript).js
@@ -0,0 +1,14 @@
+//The .forEach() Method
+
+const fruits = ['mango', 'papaya', 'pineapple', 'apple'];
+
+
+fruits.forEach(fruits => {
+  console.log(`I want to eat a ${fruits}`)
+});
+
+//Output:
+//I want to eat a mango
+//I want to eat a papaya
+//I want to eat a pineapple
+//I want to eat a apple
