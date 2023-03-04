d6d79620d7938041eb56b31568689c4b9e50e59d
Cheewathun Lerttanapit <123890144+doctoey@users.noreply.github.com>
2023-03-05 02:44:28 +0700

    add The .map() Method

--- codecademy/0study/ITERATORS(Javascript).js
+++ codecademy/0study/ITERATORS(Javascript).js
@@ -12,3 +12,29 @@ fruits.forEach(fruits => {
 //I want to eat a papaya
 //I want to eat a pineapple
 //I want to eat a apple
+
+
+//The .map() Method
+//array map เอาค่าสมาชิกใน array มา map ค่า(เปลี่ยนแปลงค่า) แล้วก็สร้างค่าเป็น array ก้อนใหม่มาใช้งาน
+const animals = ['Hen', 'elephant', 'llama', 'leopard', 'ostrich', 'Whale', 'octopus', 'rabbit', 'lion', 'dog'];
+
+// Create the secretMessage array below
+const secretMessage = animals.map(animals => {
+  return animals[0]
+})
+
+console.log(secretMessage.join(''));
+//ตัวแรกของHen ele llama leo .. มารวมกัน ได้ HelloWorld
+
+
+
+const bigNumbers = [100, 200, 300, 400, 500];
+
+// Create the smallNumbers array below
+
+const smallNumbers = bigNumbers.map(bigNumbers => {
+  return bigNumbers/100
+})
+
+console.log(smallNumbers)
+//[ 1, 2, 3, 4, 5 ]
