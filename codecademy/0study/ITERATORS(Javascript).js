b35130777a4523f3a419d757ea566a1509bffebc
Cheewathun Lerttanapit <123890144+doctoey@users.noreply.github.com>
2023-03-06 01:07:02 +0700

    add The .reduce() Method

--- codecademy/0study/ITERATORS(Javascript).js
+++ codecademy/0study/ITERATORS(Javascript).js
@@ -89,6 +89,25 @@ console.log(startsWithS)
 // });
 
 
+//The .reduce() Method
+const newNumbers = [1, 3, 5, 7];
+
+const newSum = newNumbers.reduce((accumulator, currentValue) => {
+  console.log('The value of accumulator:', accumulator);
+  console.log('The value of currentValue:', currentValue);
+  return accumulator + currentValue;
+  }, 10);
+//Output:
+//The value of accumulator: 10
+//The value of currentValue: 1
+//The value of accumulator: 11
+//The value of currentValue: 3
+//The value of accumulator: 14
+//The value of currentValue: 5
+//The value of accumulator: 19
+//The value of currentValue: 7
+console.log(newSum)
+//26
 
 
 
