111b47cd06c5a751a5cfd024db7ca3c1f15ec1c3
Cheewathun Lerttanapit <123890144+doctoey@users.noreply.github.com>
2023-03-12 00:36:17 +0700

    add ex28-31

--- JavaScript basic/230310jshero.js
+++ JavaScript basic/230310jshero.js
@@ -21,3 +21,44 @@ function normalize(para) {
 ไม่ใช่แค่ตัวอักษรแรกที่พบ แต่ทุกตัวอักษรที่ตรงกันทั้งหมด
 */
 
+
+/*ex.28 
+Write a function add that takes two numbers and returns their sum.
+
+Example: add(1, 2) should return 3.
+*/
+let add = (x, y) => x + y
+add(1, 2) //3
+
+
+/*ex.29
+Which value does x have after execution of the following code?
+let x = 3;
+x++;          //4
+x = x * 2;      //8
+x--;    //7
+*/
+
+/* ex.30 Fahrenheit Write a function toFahrenheit that converts a temperature from Celsius to Fahrenheit.
+
+Example: toFahrenheit(0) should return 32.
+*/
+let toFahrenheit = (x) => x*9/5+32
+
+
+/* ex.31 Modulo
+ Write a function onesDigit that takes a natural number and returns the ones digit of that number.
+
+Example: onesDigit(2674) should return 4.
+*/
+function onesDigit(x) {
+  return x % 10;
+}
+
+/* ex.32 Parentheses
+Write a function mean that takes 2 numbers and returns their mean value.
+
+Example: mean(1, 2) should return 1.5.
+*/
+
+let mean = (x, y) => (x+y)/2
