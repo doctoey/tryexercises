2d0839a35be11d70623241e21b2103c4bcf9c2bc
Cheewathun Lerttanapit <123890144+doctoey@users.noreply.github.com>
2023-03-13 02:32:28 +0700

    Update 230313edabit.js

--- JavaScript basic/230313edabit.js
+++ JavaScript basic/230313edabit.js
@@ -5,9 +5,17 @@ function calcAge(age) {
 
 /*
 calcAge(65) ➞ 23725
-
 calcAge(0) ➞ 0
-
 calcAge(20) ➞ 7300
 */
 
+//ex.2
+function cubes(a) {
+	return a ** 3
+}
+
+/*
+cubes(3) ➞ 27
+cubes(5) ➞ 125
+cubes(10) ➞ 1000
+*/
