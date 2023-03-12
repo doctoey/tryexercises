634f6ffa5ffa8b02939f980570c116ad1e5a99ab
Cheewathun Lerttanapit <123890144+doctoey@users.noreply.github.com>
2023-03-12 23:49:24 +0700

    ex.50

--- JavaScript basic/230312jshero.js
+++ JavaScript basic/230312jshero.js
@@ -33,3 +33,61 @@ addWithSurcharge(10, 11) returns 24.
 addWithSurcharge(11, 11) returns 26.
 addWithSurcharge(15, 27) returns 46.
 */
+
+/*ex.50 else if
+Write a function addWithSurcharge that adds two amounts with surcharge. 
+For each amount less than or equal to 10, the surcharge is 1. 
+For each amount greater than 10 and less than or equal to 20, 
+the surcharge is 2. For each amount greater than 20, the surcharge is 3.
+
+Example: addWithSurcharge(10, 30) should return 44.
+*/
+
+let addWithSurcharge3 = (amount1, amount2)  => {
+  let surcharge1 = (amount1 <= 10) ? 1 : (amount1 <= 20) ? 2 : 3;
+  let surcharge2 = (amount2 <= 10) ? 1 : (amount2 <= 20) ? 2 : 3;
+  return amount1 + amount2 + surcharge1 + surcharge2;
+}
+
+
+let addWithSurcharge4 = (amount1, amount2)  => {
+  let surcharge1, surcharge2;
+  
+  if (amount1 <= 10) {
+    surcharge1 = 1;
+  } else if (amount1 <= 20) {
+    surcharge1 = 2;
+  } else {
+    surcharge1 = 3;
+  }
+  
+  if (amount2 <= 10) {
+    surcharge2 = 1;
+  } else if (amount2 <= 20) {
+    surcharge2 = 2;
+  } else {
+    surcharge2 = 3;
+  }
+  
+  return amount1 + amount2 + surcharge1 + surcharge2;
+}
+
+/*
+addWithSurcharge(1, 1) returns 4.
+addWithSurcharge(10, 10) returns 22.
+addWithSurcharge(11, 10) returns 24.
+addWithSurcharge(10, 11) returns 24.
+addWithSurcharge(11, 11) returns 26.
+addWithSurcharge(10, 20) returns 33.
+addWithSurcharge(20, 10) returns 33.
+addWithSurcharge(10, 21) returns 35.
+addWithSurcharge(21, 10) returns 35.
+addWithSurcharge(11, 20) returns 35.
+addWithSurcharge(20, 11) returns 35.
+addWithSurcharge(11, 21) returns 37.
+addWithSurcharge(21, 11) returns 37.
+addWithSurcharge(20, 20) returns 44.
+addWithSurcharge(21, 20) returns 46.
+addWithSurcharge(20, 21) returns 46.
+addWithSurcharge(21, 21) returns 48.
+*/
