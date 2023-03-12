3bb8b5d8583a81be8aafea1f194a95a810acb91c
Cheewathun Lerttanapit <123890144+doctoey@users.noreply.github.com>
2023-03-13 03:04:14 +0700

    Update 230313edabit.js

--- JavaScript basic/230313edabit.js
+++ JavaScript basic/230313edabit.js
@@ -19,3 +19,33 @@ cubes(3) ➞ 27
 cubes(5) ➞ 125
 cubes(10) ➞ 1000
 */
+
+//ex.3
+function printArray(number) {
+  var newArray = [];
+
+  for(var i = 1; i <= number; i++) {
+    newArray.push(i);
+  }
+
+  return newArray;
+}
+/*
+printArray(1) ➞ [1]
+
+printArray(3) ➞ [1, 2, 3]
+
+printArray(6) ➞ [1, 2, 3, 4, 5, 6]
+*/
+/*
+Test Passed: Value == '[1]'
+Test Passed: Value == '[1, 2]'
+Test Passed: Value == '[1, 2, 3]'
+Test Passed: Value == '[1, 2, 3, 4]'
+Test Passed: Value == '[1, 2, 3, 4, 5]'
+Test Passed: Value == '[1, 2, 3, 4, 5, 6]'
+Test Passed: Value == '[1, 2, 3, 4, 5, 6, 7]'
+Test Passed: Value == '[1, 2, 3, 4, 5, 6, 7, 8]'
+Test Passed: Value == '[1, 2, 3, 4, 5, 6, 7, 8, 9]'
+Test Passed: Value == '[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]'
+*/
