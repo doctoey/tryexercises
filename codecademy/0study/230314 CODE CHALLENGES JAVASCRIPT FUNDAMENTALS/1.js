7663d041d4dc42ec233bf0ca758642a31ee3b80f
doctoey <reactions58@gmail.com>
2023-03-14 02:57:39 +0700

    add 123

---
+++ codecademy/0study/230314 CODE CHALLENGES JAVASCRIPT FUNDAMENTALS/1.js
@@ -0,0 +1,18 @@
+// Write your function here:
+let canIVote = (age) => {
+    if (age >= 18){
+      return true
+    }
+    return false
+  }
+  
+  
+  
+  
+  
+  // Uncomment the line below when you're ready to try out your function
+  // console.log(canIVote(19)) // Should print true
+  
+  // We encourage you to add more function calls of your own to test your code!
+  console.log(canIVote(18)) //true
+  console.log(canIVote(17)) //false
---
+++ codecademy/0study/230314 CODE CHALLENGES JAVASCRIPT FUNDAMENTALS/2.js
@@ -0,0 +1,19 @@
+// Write your function here:
+let agreeOrDisagree = (a, b) => {
+    if (a === b) {
+      return 'You agree!'
+    }
+     return 'You disagree!'
+   }
+   
+   
+   
+   
+   
+   // Uncomment the line below when you're ready to try out your function
+   // console.log(agreeOrDisagree("yep", "yep")) 
+   // Should print 'You agree!'
+   
+   // We encourage you to add more function call of your own to test your code!
+   
+   console.log(agreeOrDisagree(1,1))  //You agree!
