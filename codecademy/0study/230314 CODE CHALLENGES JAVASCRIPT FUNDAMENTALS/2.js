272a6ba6598ea3c274f07bf751c22b4dbc88b57f
Cheewathun Lerttanapit <123890144+doctoey@users.noreply.github.com>
2023-03-16 01:31:32 +0700

    Math.ceil ตัวอย่าง

--- codecademy/0study/230314 CODE CHALLENGES JAVASCRIPT FUNDAMENTALS/2.js
+++ codecademy/0study/230314 CODE CHALLENGES JAVASCRIPT FUNDAMENTALS/2.js
@@ -4,7 +4,6 @@ let truthyOrFalsy = (value) =>{
    return true
  } 
    return false
- 
 }
 truthyOrFalsy(0) //true
 console.log(truthyOrFalsy()) //false
@@ -12,7 +11,17 @@ console.log(truthyOrFalsy()) //false
 
 //numImaginaryFriends()
 let numImaginaryFriends = (person) => {
- return Math.ceil(person * 0.25)
+ return Math.ceil(person * 0.25) // ปัดขึ้นเป็นเลขจำนวนเต็ม
 }
 console.log(numImaginaryFriends(20))  //5
 console.log(numImaginaryFriends(10)) //3 (2.5 rounded up!)
+
+
+console.log(Math.ceil(.95));
+// Expected output: 1
+console.log(Math.ceil(4));
+// Expected output: 4
+console.log(Math.ceil(7.004));
+// Expected output: 8
+console.log(Math.ceil(-7.004));
+// Expected output: -7
