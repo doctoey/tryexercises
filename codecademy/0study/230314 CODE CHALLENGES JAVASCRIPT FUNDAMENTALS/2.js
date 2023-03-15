5844db45efdb572290030238e32e8e20afdcfadb
Cheewathun Lerttanapit <123890144+doctoey@users.noreply.github.com>
2023-03-16 01:30:25 +0700

    numImaginaryFriends()

--- codecademy/0study/230314 CODE CHALLENGES JAVASCRIPT FUNDAMENTALS/2.js
+++ codecademy/0study/230314 CODE CHALLENGES JAVASCRIPT FUNDAMENTALS/2.js
@@ -8,3 +8,11 @@ let truthyOrFalsy = (value) =>{
 }
 truthyOrFalsy(0) //true
 console.log(truthyOrFalsy()) //false
+
+
+//numImaginaryFriends()
+let numImaginaryFriends = (person) => {
+ return Math.ceil(person * 0.25)
+}
+console.log(numImaginaryFriends(20))  //5
+console.log(numImaginaryFriends(10)) //3 (2.5 rounded up!)
