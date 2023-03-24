ff48ffe970116945511b31ac89b116b5aba580b8
Cheewathun Lerttanapit <reactions58@gmail.com>
2023-03-24 20:48:19 +0700

    promise k'pun

---
+++ JavaScript ES6/230324promise.js
@@ -0,0 +1,30 @@
+const prom = (params) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if(params === true){
+            resolve('hello world')
+        } else {
+            reject('Error')
+        }
+        }, 1000)
+    })
+}
+
+const handleSuccess = (data) => {
+    console.log(data);
+}
+
+const handleFailure = (data) => {
+    console.log('this is a failure');
+    console.log(data);
+}
+
+// then รับ params 2 ตัว คือ
+// 1 รับ resolve
+// 2 รับ reject
+
+prom(true).then(handleSuccess, handleFailure) // hello world
+prom(false).then(handleSuccess, handleFailure) //this is a failure  , Error
+
+//เหมือนบรรทัด 27
+prom(false).then(handleSuccess).catch(handleFailure) //this is a failure  , Error
