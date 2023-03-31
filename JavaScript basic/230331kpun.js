e145092a6866e1056566405457313157458ea09a
Cheewathun Lerttanapit <reactions58@gmail.com>
2023-03-31 19:39:28 +0700

    create tutor p pun

---
+++ JavaScript basic/230331kpun.js
@@ -0,0 +1,6 @@
+//map
+//map การนำทางของใน array ออกมาทีละชิ้น แล้วไปผ่าน function ที่เป็น parameter ของ map
+
+const array = [1, 2, 3]
+//syntax
+//array.map(callbackfunction)    //ด้านในต้องใส่เป็น function
