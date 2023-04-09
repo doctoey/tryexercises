58a8158242a6a065df3cc55d2370360f8c66e91d
Cheewathun Lerttanapit <reactions58@gmail.com>
2023-04-09 20:09:02 +0700

    06 not done yet

--- JavaScript basic/230409/6/index.js
+++ JavaScript basic/230409/6/index.js
@@ -1,7 +1,23 @@
 function numbersSum(arr) {
 
   // Your code here
-
+  
 }
 
 module.exports = numbersSum;
+
+
+//# numbersSum
+
+// Arrays can be mixed with various types. 
+// Your task for this challenge is to sum all the number elements in the given array. 
+// Create a function that takes an array and returns the sum of all numbers in the array.
+
+// ```js
+// Examples:
+// numbersSum([1, 2, "13", "4", "645"]) ➞ 3
+
+// numbersSum([true, false, "123", "75"]) ➞ 0
+
+// numbersSum([1, 2, 3, 4, 5, true]) ➞ 15
+// ```
