e6085f061029165e78116332845c1805409f21b4
Cheewathun Lerttanapit <reactions58@gmail.com>
2023-04-09 22:29:19 +0700

    done 06

--- JavaScript basic/230409/6/index.js
+++ JavaScript basic/230409/6/index.js
@@ -1,10 +1,20 @@
 function numbersSum(arr) {
 
   // Your code here
-  
+  let sum = 0;
+  for(let i = 0; i < arr.length; i++) {
+    if (typeof arr[i] === 'number') {
+      sum += arr[i];
+    }
+  }
+  return sum;
 }
 
 module.exports = numbersSum;
+
+console.log(numbersSum([1, 2, "13", "4", "645"])) //➞ 3
+console.log(numbersSum([true, false, "123", "75"])) //➞ 0
+console.log(numbersSum([1, 2, 3, 4, 5, true])) //➞ 15
 
 
 //# numbersSum
