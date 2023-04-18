49bf8316cdd0f32e1941c65fb99a8c42d0d2da2b
Cheewathun Lerttanapit <reactions58@gmail.com>
2023-04-19 00:27:28 +0700

    ex.4-5-6

--- codecademy/0study/230419CODECHALLENGES.js
+++ codecademy/0study/230419CODECHALLENGES.js
@@ -102,3 +102,114 @@ function convertToBaby(arr) {
 const animals = ['panda', 'turtle', 'giraffe', 'hippo', 'sloth', 'human'];
 
 console.log(convertToBaby(animals)) 
+
+
+
+
+
+  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+
+
+const numbers = [5, 3, 9, 30];
+
+const smallestPowerOfTwo = arr => {
+      let results = [];
+      // The 'outer' for loop:
+      for (let i = 0; i < arr.length; i++) {
+            number = arr[i];
+
+            // The 'inner' while loop:
+            // We needed to create a new variable!
+            let j = 1;
+            while (j < number) {
+                  j = j * 2;
+            }
+            results.push(j);
+      }
+      return results
+}
+
+console.log(smallestPowerOfTwo(numbers)) 
+
+
+
+  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+  const veggies = ['broccoli', 'spinach', 'cauliflower', 'broccoflower'];
+
+  const politelyDecline = (veg) => {
+        console.log('No ' + veg + ' please. I will have pizza with extra cheese.');
+  }
+  
+  // Checkpoint 1 solutions:
+  const declineEverything = arr => {
+    arr.forEach(politelyDecline)
+  }
+  
+  /*
+  // As a function declaration:
+  function declineEverything(arr) {
+        arr.forEach(politelyDecline)
+  }
+  */
+  
+  
+  // Checkpoint 2 solutions:
+  // Using an anonymous function and string interpolation:
+  const acceptEverything = arr => {
+    arr.forEach(e => {
+      console.log(`Ok, I guess I will eat some ${e}.`)
+    })
+  }
+  
+  /*
+  // Using a named function and string concatenation:
+  const grudginglyAccept = veg => {
+        console.log('Ok, I guess I will eat some ' + veg + '.');
+  }
+  const acceptEverything = arr => {
+        arr.forEach(grudginglyAccept)
+  }
+  
+  // Using a loop:
+  const acceptEverything = arr => {
+   for(let i = 0; i<arr.length; i++){
+      console.log(`Ok, I guess I will eat some ${arr[i]}.`)
+   }
+  }
+  */
+
+
+
+  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+  const numbers = [2, 7, 9, 171, 52, 33, 14];
+
+  const toSquare = num => num * num
+  
+  const squareNums = arr => arr.map(toSquare) 
+  
+  /*
+  // Using an anonymous function:
+  const squareNums = arr => arr.map(e => e * e)
+  
+  // As a function declaration plus using our named function:
+  function squareNums(arr) {
+      return arr.map(toSquare)
+  }
+  */
+
+
+
+  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
