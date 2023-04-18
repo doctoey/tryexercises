cdef0c1247284e528d1aa2e5a4c9e1cc9788df95
Cheewathun Lerttanapit <reactions58@gmail.com>
2023-04-19 00:21:08 +0700

    ex.3

--- codecademy/0study/230419CODECHALLENGES.js
+++ codecademy/0study/230419CODECHALLENGES.js
@@ -71,3 +71,34 @@ const greetAliens = (arr) => {
   
   greetAliens(aliens);
   
+
+
+
+  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+  const convertToBaby = arr => {
+    const babyArray = [];
+    for (let i = 0; i < arr.length; i++) {
+          babyArray.push('baby ' + arr[i]);
+    }
+    return babyArray
+}
+
+/*
+// As a function declaration:
+function convertToBaby(arr) {
+    const babyArray = [];
+    for (let i = 0; i < arr.length; i++) {
+          babyArray.push('baby ' + arr[i]);
+    }
+    return babyArray
+}
+*/ 
+
+const animals = ['panda', 'turtle', 'giraffe', 'hippo', 'sloth', 'human'];
+
+console.log(convertToBaby(animals)) 
