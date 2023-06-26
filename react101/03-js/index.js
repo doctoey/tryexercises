1d64e13a01c638b566631847570a1d2cddf91e88
doctoey <reactions58@gmail.com>
2023-06-26 10:04:01 +0700

    basic 260623

---
+++ react101/03-js/index.js
@@ -0,0 +1,11 @@
+console.log('Helo soooserooroeo');
+// alert('Helo soooserooroeo');
+// prompt('Helo soooserooroeo');
+
+// document.getElementById('app').innerHTML = 'APPPPPPPPPPPPP';
+
+// document.createElement('div');
+// // appendChild('div');
+// document.getElementById('jaaaa').appendChild('div');
+// document.getElementById('jaaaa').innerHTML = 'APPPPPPPPPPPPP';
+
