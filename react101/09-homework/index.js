7b899ac2e6f85b3ed99dbda7c4fb30ccdf364ca3
doctoey <reactions58@gmail.com>
2023-06-27 18:00:33 +0700

    backup

--- react101/09-homework/index.js
+++ react101/09-homework/index.js
@@ -7,10 +7,60 @@
 // 100 -> หนึ่งร้อย
 // 1000 -> หนึ่งพัน
 
-function convert(num) {
-    
-}
+function convertToThaiText(num) {
+    var numArr = ['ศูนย์', 'หนึ่ง', 'สอง', 'สาม', 'สี่', 'ห้า', 'หก', 'เจ็ด', 'แปด', 'เก้า'];
+    var posArr = ['', 'สิบ', 'ร้อย', 'พัน', 'หมื่น', 'แสน', 'ล้าน'];
+  
+    if (num === 0) {
+      return numArr[0];
+    }
+  
+    var res = '';
+    var hasTen = false;
+  
+    if (num >= 10 && num <= 99) {
+      hasTen = true;
+    }
+  
+    for (var i = 6; i >= 0; i--) {
+      var divisor = Math.pow(10, i * 4);
+      if (num >= divisor) {
+        var quotient = Math.floor(num / divisor);
+        res += convertToThaiText(quotient) + posArr[i];
+        num %= divisor;
+      }
+    }
+  
+    if (hasTen) {
+      var ten = Math.floor(num / 10);
+      if (ten === 1 && num % 10 === 1) {
+        res += 'สิบเอ็ด';
+      } else if (ten === 2 && num % 10 === 1) {
+        res += 'ยี่สิบเอ็ด';
+      } else if (ten === 2) {
+        res += 'ยี่สิบ';
+      } else {
+        res += numArr[ten] + 'สิบ';
+      }
+      num %= 10;
+    }
+  
+    if (num > 0) {
+      res += numArr[num];
+    }
+  
+    return res;
+  }
+  
 
+console.log(convertToThaiText(1)); // หนึ่ง
+console.log(convertToThaiText(10)); // สิบ
+console.log(convertToThaiText(11)); // สิบเอ็ด
+console.log(convertToThaiText(21)); // ยี่สิบเอ็ด
+console.log(convertToThaiText(100)); // หนึ่งร้อย
+console.log(convertToThaiText(1000)); // หนึ่งพัน
+  
 module.exports = {
-    convert
-}
+    convertToThaiText
+};
+  
--- react101/09-homework/index.test.js
+++ react101/09-homework/index.test.js
@@ -1,5 +1,13 @@
-const { convert } = require('./index.js')
+const { convertToThaiText } = require('./index.js')
 
-describe('convert', function () {
-    
+describe('convertToThaiText', function () {
+    it('should convert numbers to Thai text correctly', function () {
+        expect(convertToThaiText(1)).toBe('หนึ่ง');
+        expect(convertToThaiText(10)).toBe('สิบ');
+        expect(convertToThaiText(11)).toBe('สิบเอ็ด');
+        expect(convertToThaiText(21)).toBe('ยี่สิบเอ็ด');
+        expect(convertToThaiText(100)).toBe('หนึ่งร้อย');
+        expect(convertToThaiText(1000)).toBe('หนึ่งพัน');
+
+      });
 })
