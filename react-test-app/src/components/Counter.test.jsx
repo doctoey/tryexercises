7a1cd420e65037fdd9173becfb2cbfcb631bb6ff
doctoey <doctoey@odds.team>
2025-05-06 15:48:41 +0700

    feat: Add Counter component with increment and decrement functionality and corresponding tests

---
+++ react-test-app/src/components/Counter.test.jsx
@@ -0,0 +1,33 @@
+import { describe, it, expect, beforeEach } from "vitest";
+import { render, screen, fireEvent } from "@testing-library/react";
+
+import Counter from "./Counter";
+
+describe("test Counter component", () => {
+  // Counter render ออกมาได้จริงๆใช่ไหม (มั่นใจใช่ไหมว่าไม่มี error อะไรตอนจังหวะ render)
+  // ทำอะไรก่อน ในทุกๆการ run test
+  // เลย render Counter component ขึ้นมาก่อน
+  // ทุกครั้งที่มีการ run test จะ render <Counter /> เสมอ
+  beforeEach(() => {
+    render(<Counter />);
+  });
+
+  // test case
+  // function ของตัวนั้น
+  it("should render counter", () => {
+    // พึ่งพา screen ทำการหา dom
+    // check Counter: มีอยู่จริง คือ toBeInTheDocument
+    expect(screen.getByText(/Counter:/)).toBeInTheDocument();
+  });
+
+  it("increments counter", () => {
+    // fireEvent click จำลองการ click ออกมา
+    fireEvent.click(screen.getByText("Increment"));
+    expect(screen.getByText("Counter: 1")).toBeInTheDocument();
+  });
+
+  it("decrement counter", () => {
+    fireEvent.click(screen.getByText("Decrement"));
+    expect(screen.getByText("Counter: -1")).toBeInTheDocument();
+  });
+});
